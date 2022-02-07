19764fc5b081c6f10fb0b67d31262333fdfe2b87
ibianche <inna.pomorska@gmail.com>
2022-02-07 15:42:38 +0100

    class Product

---
+++ src/js/components/Product.js
@@ -0,0 +1,36 @@
+import {select, templates} from '../settings.js';
+import {utils} from "../utils";
+
+class Product {
+
+  constructor(id, data){
+    const thisProduct = this;
+
+    thisProduct.id = id;
+    thisProduct.data = data;
+
+    thisProduct.renderInMenu();
+  }
+
+  renderInMenu(){
+    const thisProduct = this;
+
+
+    const generatedHTML = templates.coffeeProduct(thisProduct.data);
+
+
+    thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+
+    const productContainer = document.querySelector(select.containerOf.product);
+
+
+    productContainer.appendChild(thisProduct.element);
+
+
+
+  }
+
+}
+
+export default Product;
--- src/js/settings.js
+++ src/js/settings.js
@@ -3,8 +3,19 @@ export const select = {
   templateOf: {
     product: '#template-products',
   },
-}
+  containerOf: {
+    product: '.coffee-description'
+  },
+};
 
 export const templates = {
-  menuProduct: Handlebars.compile(document.querySelector(select.templateOf.product).innerHTML),
-}
+  coffeeProduct: Handlebars.compile(document.querySelector(select.templateOf.product).innerHTML),
+};
+
+export const settings = {
+  db: {
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
+    products: 'products',
+  }
+
+};
