821ae33d18ac2cbb115f42c3122b239410c3dcc2
ibianche <inna.pomorska@gmail.com>
2022-02-07 16:26:55 +0100

    Modification in initProduct

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import { settings, select, templates } from './settings.js'
+import { settings } from './settings.js'
 import Product from './components/Product.js';
 
 
@@ -13,6 +13,16 @@ export const app = {
       .then((parsedResponse) => {
         this.data.products = parsedResponse;
       });
+  },
+
+  initProduct: function(){
+    const thisApp = this;
+
+    for (let productData in thisApp.data.products) {
+      // new Product(productData, thisApp.data.products[productData]);
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+
   },
 
   init: function() {
